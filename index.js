82fdaddbc95ca5ea77916bf343c237bf1ced129d
Brittany Petersen <britt.petersen100@gmail.com>
2022-02-23 20:55:58 -0700

    completed 19 and 20 tests

--- index.js
+++ index.js
@@ -186,7 +186,11 @@ class Car {
  * })
  */
 function isEvenNumberAsync(number) {
-  // ✨ implement
+  if (number % 2 == 0) {
+    return 'true'
+  } else {
+    return 'false'
+  }
 }
 
 module.exports = {
